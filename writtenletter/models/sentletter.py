138793937dbff17c73615f8aa84ca0b6bba74dee
kazuki <kitano-kazuki954@e.c..u-tokyo.ac.jp>
2024-09-23 13:34:20 +0900

    added id to sent letter model

--- writtenletter/models/sentletter.py
+++ writtenletter/models/sentletter.py
@@ -1,12 +1,15 @@
 from django.db import models
 from django.utils import timezone
+import uuid
+
 from accounts.models import User
 from writtenletter.models import WrittenLetter
 
 class SentLetter(models.Model):
-    received_user = models.OneToOneField(User, related_name='received_letters', on_delete=models.CASCADE)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    received_user = models.ForeignKey(User, related_name='received_letters', on_delete=models.CASCADE)
     received_at = models.DateTimeField(default=timezone.now)
-    letter = models.OneToOneField(WrittenLetter, related_name='sent_letter', on_delete=models.CASCADE, primary_key=True)
+    letter = models.ForeignKey(WrittenLetter, related_name='sent_letter', on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.received_user.email}'s sent letter from {self.letter.user.email} - {self.received_at}"
