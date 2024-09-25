4ceb44ff01d3bb08b3cf599d57a92123062ccb26
kazuki <kitano-kazuki954@e.c..u-tokyo.ac.jp>
2024-09-25 19:17:06 +0900

    unsent letterをわかりやすくかえすようにした

--- accounts/views/user_views.py
+++ accounts/views/user_views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
 from django.contrib.auth import get_user_model
-from accounts.serializers import UserSerializer
+from accounts.serializers import UserSerializer, ProfileSerializer
 from rest_framework.response import Response
 
 from rest_framework.decorators import action
@@ -13,6 +13,26 @@ User = get_user_model()
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    @action(detail=True, methods=['GET'])
+    def unchecked_letters(self, request, pk=None):
+        user = self.get_object()
+        letters = user.received_letters.filter(checked=False).order_by('-received_at')
+        serializer = SentLetterSerializer(letters, many=True)
+
+        objects = []
+
+        for letter in letters:
+            received_user_profile = letter.letter.user.profile
+            prof_serializer = ProfileSerializer(received_user_profile)
+            result = {
+                "letter" : letter.letter.content,
+                "from" : prof_serializer.data,
+                "at" : letter.received_at
+            }
+            objects.append(result)
+        # letters.update(checked=True)
+        return Response(objects)
 
     @action(detail=True, methods=['GET'])
     def letters(self, request, pk=None):
